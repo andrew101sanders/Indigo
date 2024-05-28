c9cb0385616662670834e31473f15e0fd144882c
andrew101sanders <andrew101sanders@gmail.com>
2024-05-28 03:41:11 -0400

    Fixed minor path issues

--- GetAllLinesOfCode.py
+++ GetAllLinesOfCode.py
@@ -14,6 +14,6 @@ def list_files_in_directory_to_textfile(directory, output_file):
                     outfile.write('\n\n')
 
 # Example usage
-directory = 'F:\General_Project_Repositories\Indigo\GameEngine\src'  # Replace with your directory path
+directory = 'F:\\General_Project_Repositories\\Indigo\\Indigo\\src'  # Replace with your directory path
 output_file = 'output.txt'  # Replace with your desired output file name
 list_files_in_directory_to_textfile(directory, output_file)
